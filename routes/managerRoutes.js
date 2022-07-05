aec0fae02f29578e9e6a18edc224ce8d9c2c7e3a
Danusha <danush99online@gmail.com>
2022-07-05 13:09:06 +0530

    create a search in Supervsor allocation and finish

--- routes/managerRoutes.js
+++ routes/managerRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/view_user/:user_id', manager.viewUser);
 router.get('/get_users_list', manager.getUserList);
+router.get('/get_supervisor_list', manager.get_supervisor_list);
 router.post('/edit_user/:user_id', manager.editUser);
 
 module.exports = router;
