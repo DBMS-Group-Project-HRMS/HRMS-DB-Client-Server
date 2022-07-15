f97bf0be6d2052926724e0cdd72f6f2df5514188
Thivindu Paranayapa <paranayapathivindu@gmail.com>
2022-07-15 13:35:26 +0530

    Supervisor view user and apply leave enabled

--- routes/managerRoutes.js
+++ routes/managerRoutes.js
@@ -3,7 +3,7 @@ const manager = require('../controllers/manager');
 const {verifyToken, hasPaygrade}  = require("../middleware/auth");
 const router = express.Router();
 
-router.get('/view_user/:user_id', hasPaygrade(['level 1', 'level 3', 'level 4']), manager.viewUser);
+router.get('/view_user/:user_id', hasPaygrade(['level 1', 'level 2', 'level 3', 'level 4']), manager.viewUser);
 router.get('/get_users_list', hasPaygrade(['level 3', 'level 4']), manager.getUserList);
 router.get('/get_supervisor_list', manager.get_supervisor_list);
 router.get('/get_supervisor/:user_id', manager.get_supervisor);
